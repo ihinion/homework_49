ccf450729f9c894bbcbbca8eb8c34b2c3f351a5f
ilia <jamiashvili@gmail.com>
2020-08-04 16:40:47 +0600

    Created task.html, TaskView, path in urls.py, url in card.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic import View, TemplateView
 from .models import Task
 
@@ -10,4 +10,15 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         tasks = Task.objects.all()
         context['tasks'] = tasks
+        return context
+
+
+class TaskView(TemplateView):
+    template_name = 'task.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        task = get_object_or_404(Task, pk=pk)
+        context['task'] = task
         return context
