3262a269a469e11b0014c2d1aa088d61eba9c20d
ilia <jamiashvili@gmail.com>
2020-08-04 15:40:25 +0600

    Created models Task, Type, Status, registered them in admin.py, migrated, added dump

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task, Status, Type
 
-# Register your models here.
+# superuser
+# login: admin
+# password: admin
+admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(Type)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('new', 'New'), ('in_progress', 'In progress'), ('done', 'Done')]
+TYPE_CHOICES = [('task', 'Task'), ('bug', 'Bug'), ('enhancement', 'Enhancement')]
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=200, null=False, blank=False, verbose_name='Description')
+    detailed_desc = models.TextField(max_length=1500, null=True, blank=True, verbose_name='Detailed description')
+    status = models.ForeignKey('webapp.Status', related_name='status_set', on_delete=models.PROTECT,
+                               verbose_name='Status')
+    type = models.ForeignKey('webapp.Type', related_name='status_set', on_delete=models.PROTECT,
+                             verbose_name='Status')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return f'{self.pk}. {self.description}'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=15, null=False, blank=False, verbose_name='Name',
+                            choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=15, null=False, blank=False, verbose_name='Name',
+                            choices=TYPE_CHOICES, default=TYPE_CHOICES[0][0])
+
+    def __str__(self):
+        return self.name
