69e72505f95db5ca2dfa72e7933ef7c85e7691b9
ilia <jamiashvili@gmail.com>
2020-08-08 16:59:01 +0600

    Changed TaskCreateView to FormView as basic, added initials to type and status fields in TaskForm

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -7,5 +7,7 @@ class TaskForm(forms.Form):
     description = forms.CharField(max_length=200, label='Description', required=True)
     detailed_desc = forms.CharField(max_length=1500, label='Detailed description', required=False,
                                     widget=widgets.Textarea)
-    status = forms.ModelChoiceField(label='Status', required=True, queryset=Status.objects.all())
-    type = forms.ModelChoiceField(label='Type', required=True, queryset=Type.objects.all())
+    status = forms.ModelChoiceField(label='Status', required=True, queryset=Status.objects.all(),
+                                    initial=Status.objects.get(name__iexact='new'))
+    type = forms.ModelChoiceField(label='Type', required=True, queryset=Type.objects.all(),
+                                  initial=Type.objects.get(name__iexact='task'))
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.views.generic import View, TemplateView
+from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.views.generic import TemplateView, FormView
 from .models import Task
 from .forms import TaskForm
 
@@ -25,17 +25,16 @@ class TaskView(TemplateView):
         return context
 
 
-class TaskCreateView(View):
-    def get(self, request):
-        return render(request, 'create.html', {'form': TaskForm()})
+class TaskCreateView(FormView):
+    template_name = 'create.html'
+    form_class = TaskForm
 
-    def post(self, request):
-        form = TaskForm(data=request.POST)
-        if form.is_valid():
-            task = Task.objects.create(**form.cleaned_data)
-            return redirect('task_view', pk=task.pk)
-        else:
-            return render(request, 'create.html', {'form': form})
+    def form_valid(self, form):
+        self.task = Task.objects.create(**form.cleaned_data)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('task_view', kwargs={'pk': self.task.pk})
 
 
 class TaskUpdateView(TemplateView):
