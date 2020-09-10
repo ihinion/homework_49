e9d8a00a7d882d1c1cd57cb92d09beb44a6ab90b
ilia <jamiashvili@gmail.com>
2020-09-11 04:09:38 +0600

    Added settings for images on the website, and settings to show profile info for 'User' model in admin panel

--- source/main/settings.py
+++ source/main/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/media/'
--- source/main/urls.py
+++ source/main/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from webapp.views import IndexView, TaskView, TaskUpdateView, TaskDeleteView, \
@@ -32,4 +34,4 @@ urlpatterns = [
     path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
     path('project/<int:pk>/users/', UpdateProjectUsers.as_view(), name='users_update'),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
