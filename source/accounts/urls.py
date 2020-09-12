3ad2ff96ca9d527809c2ced979042029be8d3dac
ilia <jamiashvili@gmail.com>
2020-09-12 14:55:37 +0600

    Added user page, UserDetailedView and links in templates

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserDetailView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='create')
+    path('register/', RegisterView.as_view(), name='create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail')
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
-from django.contrib.auth import login
-from django.views.generic import CreateView
+from django.contrib.auth import login, get_user_model
+from django.views.generic import CreateView, DetailView
 from accounts.forms import MyUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from webapp.models import Project
 
 
 class RegisterView(CreateView):
@@ -15,3 +18,13 @@ class RegisterView(CreateView):
         login(self.request, user)
         return redirect('index')
 
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        projects = Project.objects.filter(users__id=self.object.id)
+        kwargs['projects'] = projects
+        return super().get_context_data(**kwargs)
